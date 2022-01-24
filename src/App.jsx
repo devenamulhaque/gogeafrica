598ac47b4293a4c530476bf0c084b1bc25c7993b
Enamul Haque <enamul.iu91@gmail.com>
2022-01-25 02:32:40 +0600

    tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App bg-pink-600">
+      <h1 className='text-3xl font-bold underline bg-purple-400'>Heading one</h1>
+    </div>
+  )
+}
+
+export default App
